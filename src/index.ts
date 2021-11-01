64bab40cea6c9bb7558efe879c1d16963ff57f7d
Marcel <m132@users.noreply.github.com>
2021-11-01 22:26:01 +0100

    increase the default tick rate

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Server, Stage } from './model';
 import { LegacyProtocol } from './protocol/legacy';
 
 new Server()
-    .registerStage(new Stage('main').start(100))
+    .registerStage(new Stage('main').start(50))
     .registerProtocol(new LegacyProtocol());
 
 console.log('Game server started');
