9e757377285b91665bef8013a05d1b6fe9c55358
Marcel <m132@users.noreply.github.com>
2021-11-05 20:46:18 +0100

    cli: program options via commander.js

--- src/bin/server.ts
+++ src/bin/server.ts
@@ -1,10 +1,18 @@
 #!/usr/bin/env node
+import { Command } from 'commander';
+
 import { logger as parentLogger } from '../lib/log/index.js';
 import { ConsoleLoggerBackend } from '../lib/log/console.js';
 import { LegacyProtocol } from '../lib/protocol/legacy/index.js';
-import { Server, Stage } from '../lib/index.js';
+import { Server, Stage, VERSION } from '../lib/index.js';
 
+const program = new Command()
+    .version(VERSION)
+    .parse();
+
+const options = program.opts();
 const logger = parentLogger.sub('ServerCli');
+
 parentLogger.backends.push(new ConsoleLoggerBackend());
 
 new Server()
